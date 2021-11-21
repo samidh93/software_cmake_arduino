6f3d51b8cb7f0f257adbdc82535bae7f331ced56
theionSami <sami@theion.de>
2021-11-21 20:53:55 +0100

    added files

---
+++ SimpleApp/build/CMakeFiles/Main.dir/src/main.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/Main.dir/src/main.cpp.obj: \
+ C:\Users\SamiDhiab\Theion_Repos\software_cmake_arduino\SimpleApp\src\main.cpp \
+ C:\Users\SamiDhiab\Theion_Repos\software_cmake_arduino\SimpleApp\src\main.h
