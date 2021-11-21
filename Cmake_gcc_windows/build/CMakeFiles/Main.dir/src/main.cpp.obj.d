cbfc5dffbe98783facfae2b773e13147dd84ce6a
theionSami <sami@theion.de>
2021-11-21 23:37:03 +0100

    added custom cmake build

--- Cmake_gcc_windows/build/CMakeFiles/Main.dir/src/main.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Main.dir/src/main.cpp.obj: \
- C:\Users\SamiDhiab\Theion_Repos\software_cmake_arduino\Cmake_gcc_windows\src\main.cpp \
- C:\Users\SamiDhiab\Theion_Repos\software_cmake_arduino\Cmake_gcc_windows\src\main.h
